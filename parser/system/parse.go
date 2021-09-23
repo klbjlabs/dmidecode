64b4dc6d095c2a89e02c5ea803b2b0bf14d1433a
Jia Tian <klbjlabs@gmail.com>
2021-09-23 11:46:19 +0800

    fix UUID

--- parser/system/parse.go
+++ parser/system/parse.go
@@ -38,11 +38,11 @@ func uuid(data []byte, ver string) string {
 		return "Not settable"
 	}
 
-	if ver > "2.6" {
-		return fmt.Sprintf("%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
-			data[3], data[2], data[1], data[0], data[5], data[4], data[7], data[6],
-			data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15])
-	}
+	// if ver > "2.6" {
+	// 	return fmt.Sprintf("%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
+	// 		data[3], data[2], data[1], data[0], data[5], data[4], data[7], data[6],
+	// 		data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15])
+	// }
 	return fmt.Sprintf("%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
 		data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
 		data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15])
